93be70897ba509ded750e1a273d9aefed315818a
cssmatter@gmail.com <cssmatter@gmail.com>
2021-08-17 12:55:31 +0530

    Signed-off-by: cssmatter@gmail.com <cssmatter@gmail.com>

---
+++ script.js
@@ -0,0 +1,49 @@
+var counter = document.getElementById('counter');
+
+counterData = 0;
+
+counter.innerHTML = counterData;
+
+function makeCounter(countertype){
+  
+    if(countertype == 'add'){
+        changeCounter = ++counterData
+        initialTranslate = 'translateY(-100%)'
+        addTranslate = 'translateY(100%)'
+
+            if(counterData > 0){
+                counter.style.color = 'yellow'
+            }else if(counterData == 0){
+                counter.style.color = 'white'
+            }
+
+    }else if(countertype == 'subtract'){
+        changeCounter = --counterData
+        initialTranslate = 'translateY(100%)'
+        addTranslate = 'translateY(-100%)'
+
+        if(counterData < 0){
+            counter.style.color = 'red'
+        }else if(counterData == 0){
+            counter.style.color = 'white'
+        }
+    }
+
+   counter.style.transform = initialTranslate;
+   counter.style.opacity = '0';
+
+   timeOut = 80;
+
+   setTimeout(function () {
+        counter.innerHTML = changeCounter;
+        setTimeout(function () {
+            counter.style.transform = addTranslate;    
+            counter.style.opacity = '0'; 
+            setTimeout(function () {
+                counter.style.transform = 'translateY(0%)';   
+                counter.style.opacity = '1'; 
+            },timeOut)
+        },timeOut)
+   },timeOut)
+
+}
